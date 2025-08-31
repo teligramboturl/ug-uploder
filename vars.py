fb1813514de754242a15d27fcfbece857eb6fa06
Roxybon-maker <gaganmaths22@gmail.com>
2025-08-31 20:42:17 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,8 +2,8 @@ import os
 from os import environ
 
 # API Configuration
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "24663422"))
+API_HASH = os.environ.get("API_HASH", "723918463d17ff94e655e2cc53513c61")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 CREDIT = os.environ.get("CREDIT", "𓍯𝙎𝙪𝙟𝙖𝙡⚝")
@@ -13,8 +13,8 @@ DATABASE_URL = os.environ.get("DATABASE_URL", "")
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "8458169280"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "8458169280").split()]  # Default to owner ID
+OWNER_ID = int(os.environ.get("OWNER_ID", "8431889657"))
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "8431889657").split()]  # Default to owner ID
 
 # Channel Configuration
 PREMIUM_CHANNEL = "https://t.me/+W-Q51EuLf2QwYTl"
@@ -59,6 +59,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
